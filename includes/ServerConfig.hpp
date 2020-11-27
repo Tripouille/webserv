4c6fd18ed595d81199a7d7e1c3defded32987679
Etheram68 <francois681927@hotmail.fr>
2020-11-27 16:42:31 +0100

    Define class method on file serverConfig

--- includes/ServerConfig.hpp
+++ includes/ServerConfig.hpp
@@ -6,7 +6,7 @@
 /*   By: frfrey <frfrey@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 10:12:28 by frfrey            #+#    #+#             */
-/*   Updated: 2020/11/27 12:00:15 by frfrey           ###   ########lyon.fr   */
+/*   Updated: 2020/11/27 16:41:32 by frfrey           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,60 +30,87 @@ class ServerConfig
 
 	public:
 
-		ServerConfig( std::string const & path = "server.conf" );
+		explicit ServerConfig( std::string const & path = "server.conf" );
 		~ServerConfig();
 
-		string			getUser( void ) const;
-		string			getWorker( void ) const;
-		pid_t			getPid( void ) const;
-		string			getPathModules( void ) const;
-		short			getWorkerConnections( void ) const;
-		bool			getMultiAccept( void ) const;
+
+		/* Accesseur */
+		string const &							getUser( void ) const;
+		string const &							getWorker( void ) const;
+		pid_t const 							getPid( void ) const;
+		string const &							getPathModules( void ) const;
+		short const								getWorkerConnections( void ) const;
+		bool const								getMultiAccept( void ) const;
+		bool const								getSendfile( void ) const;
+		bool const								getTcpNoPush( void ) const;
+		bool const								getTcpNoDelay( void ) const;
+		int const								getKeepAliveTimeout( void ) const;
+		int const								getTypeHashMaxSize( void ) const;
+		bool const								getServerTokens( void ) const;
+		int const								getServerNameHashBucketSize( void ) const;
+		bool const								getServerNameInRedirect( void ) const;
+		map<string, string> const &				getMimeType( void ) const;
+		string const &							getDefaultType( void ) const;
+		bool const								getGzip( void ) const;
+		bool const								getGzipVary( void ) const;
+		string const &							getGzipProxied( void ) const;
+		int const								getGzipCompLevel( void ) const;
+		int const								getGzipBuffers( void ) const;
+		string const &							getGzipVersion( void ) const;
+		vector<string> const &					getGzipType( void ) const;
+		vector<int> const &						getPortListen( void ) const;
+		string const &							getPathRoot( void ) const;
+		vector<string> const &					getIndex( void ) const;
+		string const &							getServerName( void ) const;
+		map<string, vector<string> > const &	getFastcgi( void ) const;
+
+		ofstream &								getPathAccessLog( void ) const;
+		ofstream &								getPathErrorLog( void ) const;
 
 	private:
 
 		/* Header file config */
-		string						_user;					// Define user when server start
-		string						_worker;				// Number max of CPU use
-		pid_t						_pid;					// pid of process
-		string						_pathModules; 			// path config file module enable;
+		string							_user;					// Define user when server start
+		string							_worker;				// Number max of CPU use
+		pid_t							_pid;					// pid of process
+		string							_pathModules; 			// path config file module enable;
 
 		/*	Events	*/
-		short						_workerConnections;
-		bool						_multiAccept;
+		short							_workerConnections;
+		bool							_multiAccept;
 
 		/* Http */
 		/* Basic Seeting */
-		bool						_sendfile;
-		bool						_tcpNoPush;
-		bool						_tcpNoDelay;
-		int							_keepAliveTimeout;		// Define a time period during connection will open
-		int							_typeHashMaxSize;		// Define max size of hash tables
-		bool						_serverTokens;			// Enable the transmission of your server version
-		int							_serverNameHashBucketSize;
-		bool						_serverNameInRedirect;
-		map<string, string>			_mimeType;				// type file return by server
-		string						_defaultType;			// Define the type MIME default
+		bool							_sendfile;
+		bool							_tcpNoPush;
+		bool							_tcpNoDelay;
+		int								_keepAliveTimeout;		// Define a time period during connection will open
+		int								_typeHashMaxSize;		// Define max size of hash tables
+		bool							_serverTokens;			// Enable the transmission of your server version
+		int								_serverNameHashBucketSize;
+		bool							_serverNameInRedirect;
+		map<string, string>				_mimeType;				// type file return by server
+		string							_defaultType;			// Define the type MIME default
 
 		/* Log Settings*/
-		ofstream					_access_log;			// Who requetes is writting
-		ofstream					_error_log;				// Error log is writting
+		ofstream						_pathAccessLog;			// Who requetes is writting
+		ofstream						_pathErrorLog;			// Error log is writting
 
 		/* GZIP Setting */
-		bool						_gzip;					// Enable gzip compress
-		bool						_gzipVary;
-		string						_gzipProxied;
-		int							_gzipCompLevel;
-		int							_gzipBuffers;
-		int							_gzipVersion;
-		vector<string>				_gzipType;
+		bool							_gzip;					// Enable gzip compress
+		bool							_gzipVary;
+		string							_gzipProxied;
+		int								_gzipCompLevel;
+		int								_gzipBuffers;
+		string							_gzipVersion;
+		vector<string>					_gzipType;
 
 		/* Virtual Host Config */
-		vector<int>					_portListen;
-		string						_pathRoot;
-		vector<string>				_index;
-		string						_serverName;
-		map<int, vector<string> >	_fastcgi;
+		vector<int>						_portListen;
+		string							_pathRoot;
+		vector<string>					_index;
+		string							_serverName;
+		map<string, vector<string> >	_fastcgi;
 
 
 		ServerConfig( ServerConfig const & src );
--- srcs/config/ServerConfig.cpp
+++ srcs/config/ServerConfig.cpp
@@ -6,7 +6,7 @@
 /*   By: frfrey <frfrey@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 10:12:28 by frfrey            #+#    #+#             */
-/*   Updated: 2020/11/27 10:12:29 by frfrey           ###   ########lyon.fr   */
+/*   Updated: 2020/11/27 16:39:19 by frfrey           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,9 @@
 ** ------------------------------- CONSTRUCTOR --------------------------------
 */
 
-ServerConfig::ServerConfig()
+ServerConfig::ServerConfig( std::string const & path = "server.conf" )
 {
-}
 
-ServerConfig::ServerConfig( const ServerConfig & src )
-{
 }
 
 
@@ -38,25 +35,39 @@ ServerConfig::~ServerConfig()
 ** --------------------------------- OVERLOAD ---------------------------------
 */
 
-ServerConfig &				ServerConfig::operator=( ServerConfig const & rhs )
-{
-	//if ( this != &rhs )
-	//{
-		//this->_value = rhs.getValue();
-	//}
-	return *this;
-}
-
-std::ostream &			operator<<( std::ostream & o, ServerConfig const & i )
-{
-	//o << "Value = " << i.getValue();
-	return o;
-}
-
 
 /*
 ** --------------------------------- METHODS ----------------------------------
 */
+
+string const &							ServerConfig::getUser( void ) const { return "www-data"; }
+string const &							ServerConfig::getWorker( void ) const { return "auto"; }
+pid_t const 							ServerConfig::getPid( void ) const { return getPid();}
+string const &							ServerConfig::getPathModules( void ) const { return ""; }
+short const								ServerConfig::getWorkerConnections( void ) const { return 1024; }
+bool const								ServerConfig::getMultiAccept( void ) const { return false; }
+bool const								ServerConfig::getSendfile( void ) const { return true; }
+bool const								ServerConfig::getTcpNoPush( void ) const { return true; }
+bool const								ServerConfig::getTcpNoDelay( void ) const { return true; }
+int const								ServerConfig::getKeepAliveTimeout( void ) const { return 65; }
+int const								ServerConfig::getTypeHashMaxSize( void ) const { return 2048; }
+bool const								ServerConfig::getServerTokens( void ) const {return false; }
+int const								ServerConfig::getServerNameHashBucketSize( void ) const { return 64; }
+bool const								ServerConfig::getServerNameInRedirect( void ) const { return false; }
+map<string, string> const &				ServerConfig::getMimeType( void ) const { return _mimeType; }
+string const &							ServerConfig::getDefaultType( void ) const { return "application/octet-stream"; }
+bool const								ServerConfig::getGzip( void ) const { return true; }
+bool const								ServerConfig::getGzipVary( void ) const { return false; }
+string const &							ServerConfig::getGzipProxied( void ) const { return ""; }
+int const								ServerConfig::getGzipCompLevel( void ) const { return 6;}
+int const								ServerConfig::getGzipBuffers( void ) const { return 16; }
+string const &							ServerConfig::getGzipVersion( void ) const { return "1.1"; }
+vector<string> const &					ServerConfig::getGzipType( void ) const { return _gzipType; }
+vector<int> const &						ServerConfig::getPortListen( void ) const { return _portListen; }
+string const &							ServerConfig::getPathRoot( void ) const { return "www/"; }
+vector<string> const &					ServerConfig::getIndex( void ) const { return _index; }
+string const &							ServerConfig::getServerName( void ) const { return "_"; }
+map<string, vector<string> > const &	ServerConfig::getFastcgi( void ) const { return _fastcgi; }
 
 
 /* ************************************************************************** */
